f12a218abc7b9e9c7f3f6e379bbc1b1b5c914d50
Alexander2184 <97836593+Alexander2184@users.noreply.github.com>
2026-06-04 12:52:41 +0300

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,9 +1,10 @@
 import { apiFetch } from '@/shared/api';
-import { RepoDiff } from '@/widgets/review-submission/ui/ReviewDiff/ReviewMockRepository';
 import { ReviewSubmissionClient } from '@/widgets/review-submission/ui/ReviewSubmissionClient/ReviewSubmissionClient';
 
 interface ReviewSubmissionProps {
     params: {
-        submissionId: string;
+        assignmentId: string;
     };
+
+    logs: string[];
 }
